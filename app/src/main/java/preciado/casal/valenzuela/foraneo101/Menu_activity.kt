a1a0140d3d3b2cdda7453baaed72fdee2959fc9a
José Alfredo Casal <jose.casal203970@potros.itson.edu.mx>
2021-11-17 15:54:53 -0700

    Desarrollo de la pantalla de menu incompleto

---
+++ app/src/main/java/preciado/casal/valenzuela/foraneo101/Menu_activity.kt
@@ -0,0 +1,11 @@
+package preciado.casal.valenzuela.foraneo101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
