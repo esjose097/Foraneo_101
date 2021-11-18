7e3e4f7f704ba2151dca2d9c5e27bd474edf2742
José Alfredo Casal <jose.casal203970@potros.itson.edu.mx>
2021-11-17 20:50:27 -0700

    Pantalla de transportes lista, solo un detallito

---
+++ app/src/main/java/preciado/casal/valenzuela/foraneo101/Transportes.kt
@@ -0,0 +1,11 @@
+package preciado.casal.valenzuela.foraneo101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Transportes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transportes)
+    }
+}
