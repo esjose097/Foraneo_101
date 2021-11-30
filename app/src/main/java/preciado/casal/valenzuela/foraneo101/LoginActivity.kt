e4a069f2d7671ae19192182c45c7974e1f91d171
XxPanchoPanteraxX <78341976+XxPanchoPanteraxX@users.noreply.github.com>
2021-11-30 08:04:50 -0700

    Inicio sesión con correo electrónico

--- app/src/main/java/preciado/casal/valenzuela/foraneo101/LoginActivity.kt
+++ app/src/main/java/preciado/casal/valenzuela/foraneo101/LoginActivity.kt
@@ -1,17 +1,75 @@
 package preciado.casal.valenzuela.foraneo101
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import preciado.casal.valenzuela.foraneo101.databinding.ActivityLoginBinding
 
-import java.util.TimerTask
-import java.util.Timer
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth                     //Declaramos una instancia de FIREBASE
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        //setContentView(R.layout.activity_login)
 
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        auth = Firebase.auth                                    //Inicializamos la instancia FIREBASE
+
+        binding.btnIniciar.setOnClickListener {                 //Al presionar el botón iniciarsesión valida los campos
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.passordEditText.text.toString()
+
+            when{
+                mEmail.isEmpty() || mPassword.isEmpty()->{
+                    Toast.makeText(baseContext, "El campo correo o contraseña están vacíos...",
+                        Toast.LENGTH_SHORT).show()
+                }else ->{
+                    SignIn(mEmail, mPassword)
+                }
+            }
+        }
+
+        binding.btnRegistrar.setOnClickListener{                //Al presionar el botón registrarse te lleva a la pantalla de registro
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun SignIn(email:String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this,
+                OnCompleteListener<AuthResult?> { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("TAG", "signInWithEmail:success")
+                        reload()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("TAG", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //updateUI(null)
+                    }
+                })
+    }
+
+    private fun reload() {
+        val intent = Intent(this, Menu_activity::class.java)
+        this.startActivity(intent)
     }
 }
--- app/src/main/java/preciado/casal/valenzuela/foraneo101/RegistroActivity.kt
+++ app/src/main/java/preciado/casal/valenzuela/foraneo101/RegistroActivity.kt
@@ -1,11 +1,80 @@
 package preciado.casal.valenzuela.foraneo101
 
+import android.R.attr
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import preciado.casal.valenzuela.foraneo101.databinding.ActivityRegistroBinding
+import android.widget.Toast
+import android.util.Log
+import android.util.Patterns
+import java.util.regex.Pattern
 
 class RegistroActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth                     //Declaramos una instancia de FIREBASE
+    private lateinit var binding: ActivityRegistroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        //setContentView(R.layout.activity_registro)
+
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth                                    //Inicializamos la instancia FIREBASE
+
+        binding.btnBack.setOnClickListener{                     //Si da click en el botón atrás ir a la pantalla principal
+            val intent = Intent(this, LoginActivity::class.java)
+            this.startActivity(intent)
+        }
+
+        binding.btnGuardarUsuario.setOnClickListener {          //Si da click en el botón guardar validar campos y guardar en FIREBASE
+            val mEmail = binding.tvEmail.text.toString()
+            val mPassword = binding.tvPassword.text.toString()
+            val mRepeatPassword = binding.tvPassword2.text.toString()
+            val passwordRegex = Pattern.compile( "^"+
+                "(?=.*[-@#$%^&+=])" +   //Al menos 1 carácter especial
+                ".{6,}" +               //Al menos 6 caracteres
+                "$")
+
+        if (mEmail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+            Toast.makeText(this, "Ingrese un email valido.",
+                Toast.LENGTH_SHORT).show()
+        } else if (mPassword.isEmpty() || !passwordRegex.matcher(mPassword).matches()){
+            Toast.makeText(this, "La contraseña es debil.",
+                Toast.LENGTH_SHORT).show()
+        } else if (mPassword != mRepeatPassword){
+            Toast.makeText(this, "Las contraseñas no coinciden",
+                Toast.LENGTH_SHORT).show()
+        }else{
+            createAccount(mEmail, mPassword)
+        }
+
+        }
+    }
+
+    private fun createAccount(email:String, password:String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext, "Registro completo",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val intent = Intent(this, Menu_activity::class.java)
+                        this.startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    }
+                }
     }
 }
